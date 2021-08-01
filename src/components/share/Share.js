01787d7fdb7296f0956da11e50d5d4eeed93f02d
SasikaAththanaya <sasikaharshana97@gmail.com>
2021-08-01 20:39:01 +0530

    some styling was added for sharecomponent

--- src/components/share/Share.js
+++ src/components/share/Share.js
@@ -1,22 +1,36 @@
 import "./Share.css";
-import {PermMedia} from "@material-ui/icons";
+import {PermMedia,Label,Room,EmojiEmotions} from "@material-ui/icons";
+import { Button } from "@material-ui/core";
 
 function Share() {
     return (
         <div className="share">
             <div className="shareWrapper">
                 <div className="shareTop">
-                    <img className="shareProfileImg" src="./assets/person/17.jpg" alt=""/>
+                    <img className="shareProfileImg" src="./assets/person/1.jpg" alt=""/>
                     <input placeholder="What's in your mind sasi?" className="shareInput"/>
                 </div>
                 <hr className="shareHr"/>
                 <div className="shareBottom">
                     <div className="shareOptions">
                         <div className="shareOption">
-                            <PermMedia className="shareIcon"/>
+                            <PermMedia htmlColor="tomato" className="shareIcon"/>
                             <span className="shareOptionText">Photo Or Video</span>
                         </div>
+                        <div className="shareOption">
+                            <Label htmlColor="blue" className="shareIcon"/>
+                            <span className="shareOptionText">Tag</span>
+                        </div>
+                        <div className="shareOption">
+                            <Room htmlColor="green" className="shareIcon"/>
+                            <span className="shareOptionText">Location</span>
+                        </div>
+                        <div className="shareOption">
+                            <EmojiEmotions htmlColor="goldenrod" className="shareIcon"/>
+                            <span className="shareOptionText">Feelings</span>
+                        </div>
                     </div>
+                    <Button  className="shareButton">Share</Button>
                 </div>
             </div>
         </div>
