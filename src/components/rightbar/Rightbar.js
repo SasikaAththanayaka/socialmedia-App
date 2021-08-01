8e651e534fd2c5265181e96f047851e8cce377b1
SasikaAththanaya <sasikaharshana97@gmail.com>
2021-08-01 18:28:33 +0530

    topbar was added

---
+++ src/components/rightbar/Rightbar.js
@@ -0,0 +1,12 @@
+import "./Rightbar.css";
+
+
+function Rightbar() {
+    return (
+        <div className="rightbar">
+            Rightbar
+        </div>
+    )
+}
+
+export default Rightbar
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,19 @@
+import "./Sidebar.css";
+import {RssFeed} from "@material-ui/icons";
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarWrapper">
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">
+                        <RssFeed className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Feed</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
