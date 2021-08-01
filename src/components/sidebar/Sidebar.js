ffa91bf07273d6f375f0b04f716ab8f4371923ec
SasikaAththanaya <sasikaharshana97@gmail.com>
2021-08-01 19:26:33 +0530

    sidebar was added

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,5 +1,6 @@
 import "./Sidebar.css";
-import {RssFeed} from "@material-ui/icons";
+import {RssFeed,Chat,PlayCircleFilledOutlined,Group,HelpOutline,WorkOutline,Event,School,Bookmark} from "@material-ui/icons";
+import { Button } from "@material-ui/core";
 
 function Sidebar() {
     return (
@@ -9,6 +10,62 @@ function Sidebar() {
                     <li className="sidebarListItem">
                         <RssFeed className="sidebarIcon"/>
                         <span className="sidebarListItemText">Feed</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Chat className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Chats</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <PlayCircleFilledOutlined className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Videos</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Group className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Groups</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Bookmark className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Bookmarks</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <HelpOutline className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Questions</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <WorkOutline className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Jobs</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Event className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Events</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <School className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Courses</span>
+                    </li>
+                </ul>
+                <Button className="sidebarButton">Show More</Button>
+                <hr className="sidebarHr"/>
+                <ul className="sidebarFridentList">
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="./assets/person/2.jpg" alt=" "/>
+                        <span className="sidebarFriendName">Sasika Harshana</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="./assets/person/3.jpg" alt=" "/>
+                        <span className="sidebarFriendName">Sasika Harshana</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="./assets/person/4.jpg" alt=" "/>
+                        <span className="sidebarFriendName">Sasika Harshana</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="./assets/person/5.jpg" alt=" "/>
+                        <span className="sidebarFriendName">Sasika Harshana</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="./assets/person/6.png" alt=" "/>
+                        <span className="sidebarFriendName">Sasika Harshana</span>
                     </li>
                 </ul>
             </div>
